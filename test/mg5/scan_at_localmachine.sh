1f6e15993a1faf1aac052ee2fb08c81e9705ed7a
Junghwan John Goh <jhgoh@cern.ch>
2022-04-06 01:11:07 +0900

    little bit more realistic job submit emulation

--- test/mg5/scan_at_localmachine.sh
+++ test/mg5/scan_at_localmachine.sh
@@ -4,9 +4,9 @@
 #ARCHIVE=tttt_NLO4.tgz
 export SIF=/store/sw/singularity/mg5/mg5_amc_2.9.9.sif
 export PBS_O_WORKDIR=`pwd`
-export PBS_ARRAY_INDEX=0
 export PBS_JOBID=10000
-N=3
+INDEX0=0
+NJOBS=3
 
 #for NEVTS in 1 10 20 50 100 200 500 1000 2000 5000 10000 20000 50000 100000; do
 for NEVTS in 1 10 20 50 100 200 500 1000 2000 5000 10000; do
@@ -14,9 +14,9 @@ for NEVTS in 1 10 20 50 100 200 500 1000 2000 5000 10000; do
     for NCPUS in 64 48 32 24 16 12 8 4 2 1; do
         export NCPUS
         export OMP_NUM_THREADS=$NCPUS
+        export PBS_JOBID=$(($PBS_JOBID+1))
 
-        for PBS_ARRAY_INDEX in `seq $N`; do
-            export PBS_JOBID=$(($PBS_JOBID+1))
+        for PBS_ARRAY_INDEX in `seq $INDEX0 $(($INDEX0+$NJOBS-1))`; do
             export PBS_ARRAY_INDEX
             export PBS_JOBNAME=madgraph.TTTT_5f_NLO.nCPUs_${NCPUS}__nEvents_${NEVTS}.${PBS_JOBID}.pbs.`printf %04d ${PBS_ARRAY_INDEX}`
             ./generate.sh
